fbb4b47a20862d4cbd32a683b7d58f38e2332315
Srijan Sarkar <srijansarkar2705@gmail.com>
2024-09-22 16:23:22 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { BackgroundLines } from "@/components/ui/background-lines";
 
 function YourApp() {
   return (
-    <ConnectButton
-      label="Sign in"
-      accountStatus={{
-        smallScreen: "avatar",
-        largeScreen: "full",
-      }}
-    />
+    <>
+      <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+        <ConnectButton
+          label="Sign in"
+          accountStatus={{
+            smallScreen: "avatar",
+            largeScreen: "full",
+          }}
+        />
+      </BackgroundLines>
+    </>
   );
 }
 
