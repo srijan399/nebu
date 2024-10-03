25259e8071a5c4df525a17e3e0b809f5bb36ffb3
Prayas <prayaspal04@gmail.com>
2024-10-03 20:49:08 +0530

    updated and deployed contract

--- web3/contracts/NameFave.sol
+++ web3/contracts/NameFave.sol
@@ -8,16 +8,15 @@ contract NameAndFavoriteNumber {
     }
 
     // Mapping to store user data based on the user's address
-    mapping(address => UserData) public users;
+    mapping(address => UserData[]) public users;
 
     // Function to set user's name and favorite number
     function setUserData(string memory _name, uint _favoriteNumber) public {
-        users[msg.sender] = UserData(_name, _favoriteNumber);
+        users[msg.sender].push(UserData(_name, _favoriteNumber));
     }
 
     // Function to get user's name and favorite number
-    function getUserData(address _user) public view returns (string memory, uint) {
-        UserData memory user = users[_user];
-        return (user.name, user.favoriteNumber);
+    function getUserData(address _user) public view returns (UserData[] memory) {
+        return users[_user];
     }
 }
