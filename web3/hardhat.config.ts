5f6a5c1dff542359842bb9cdefa121b4926f7620
Prayas <prayaspal04@gmail.com>
2024-10-14 04:06:46 +0530

    verification

--- web3/hardhat.config.ts
+++ web3/hardhat.config.ts
@@ -1,8 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "dotenv/config";
 import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-verify";
 
-const { RPC_URL, PRIVATE_KEY } = process.env;
+const { RPC_URL, PRIVATE_KEY, ETHERSCAN_API } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
@@ -20,6 +21,27 @@ const config: HardhatUserConfig = {
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
   },
+  etherscan: {
+    // apiKey: "U9CTZEN3RYUF7MAMHE1DIDVGS2HK9V36U6",
+    apiKey: {
+      'base-sepolia': 'ETHERSCAN_API',
+      'amoy': 'ETHERSCAN_API',
+      'sepolia': 'ETHERSCAN_API',
+    },
+    customChains: [
+      {
+        network: "base-sepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://base-sepolia.blockscout.com/api",
+          browserURL: "https://base-sepolia.blockscout.com"
+        }
+      }
+    ]
+  },
+  sourcify: {
+    enabled: true
+  }
 };
 
 export default config;
